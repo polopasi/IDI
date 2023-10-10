8a9d866293cf36ed5e922e354e83149426ad4ab9
polopasi <polgarciver@gmail.com>
2023-10-10 21:51:46 +0200

    exercici3

---
+++ Bloc1/Sessio3/Exercici2/MyGLWidget.h
@@ -0,0 +1,52 @@
+#include <QOpenGLFunctions_3_3_Core>
+#include <QOpenGLWidget>
+#include <QOpenGLShader>
+#include <QOpenGLShaderProgram>
+#include <QKeyEvent>  //QKeyEvent SE DECLARA ANTES QUE LIBS DE GLM
+#define GLM_FORCE_RADIANS
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+class MyGLWidget : public QOpenGLWidget, protected QOpenGLFunctions_3_3_Core
+{
+  Q_OBJECT
+
+  public:
+    MyGLWidget (QWidget *parent=0);
+    ~MyGLWidget ();
+    
+  protected:
+    // initializeGL - Aqui incluim les inicialitzacions del contexte grafic.
+    virtual void initializeGL ();
+
+    // paintGL - Mètode cridat cada cop que cal refrescar la finestra.
+    // Tot el que es dibuixa es dibuixa aqui.
+    virtual void paintGL ();
+ 
+    // resize - Es cridat quan canvia la mida del widget
+    virtual void resizeGL (int width, int height);  
+
+    //Declaro mi función de keyPressEvent
+    virtual void keyPressEvent ( QKeyEvent * e );
+
+    void modelTransform();
+
+  private:
+    void creaBuffers ();
+    void carregaShaders ();
+    
+    // attribute locations
+    GLuint vertexLoc;
+    GLuint colorLoc;
+
+    GLuint transLoc;    //Matriz transformación Location
+    
+    // Program
+    QOpenGLShaderProgram *program;
+
+    GLuint VAO1;
+    GLint ample, alt;
+
+    float tx, ty;
+    float angle = 0;
+};
